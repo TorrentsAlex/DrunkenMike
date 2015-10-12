a46aafa9c96e54c58511d41dd42476e4139bc0d4
alex torrents <torrents.alex1@gmail.com>
2015-10-12 13:49:02 +0200

    Rename the class for TurriGame and add singleton pattern

--- core/src/com/turri/game/TurriGame.java
+++ core/src/com/turri/game/TurriGame.java
@@ -3,24 +3,32 @@ package com.turri.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.turri.inputs.TurriListener;
 import com.turri.interfaces.imageManagerInterface;
 import com.turri.manager.ImageManager;
 
-public class GdxGame extends ApplicationAdapter implements imageManagerInterface {
+public class TurriGame extends ApplicationAdapter implements imageManagerInterface {
 	SpriteBatch batch;
 	ImageManager manager;
 
 	boolean imagesLoaded = false;
+	private static TurriGame game;
 
+	public static TurriGame sharedGame() {
+		if (game == null) {
+			game = new TurriGame();
+		}
+		return game;
+	}
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
-
 		manager = new ImageManager(this);
 		manager.loadImages(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.input.setInputProcessor(new GestureDetector(new TurriListener()));
 	}
 
 	@Override
@@ -33,9 +41,12 @@ public class GdxGame extends ApplicationAdapter implements imageManagerInterface
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		manager.drawBackground(batch);
-		manager.drawFarmer();
 		batch.end();
 
+		// Draw sprites after end batch, the sprites uses other batch to render the texture
+		manager.drawFarmer();
+
+		// Update positions
 		manager.updateBackground();
 	}
 
