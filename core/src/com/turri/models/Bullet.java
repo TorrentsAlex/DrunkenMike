e6b757e3962c77273ab0b986929f142d23944e43
alex torrents <torrents.alex1@gmail.com>
2015-11-01 13:28:06 +0100

    Fix trajectory and rotate the texture

--- core/src/com/turri/models/Bullet.java
+++ core/src/com/turri/models/Bullet.java
@@ -4,21 +4,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Bullet {
 	private float x;
 	private float y;
 	private float finalX;
 	private float finalY;
-	
-	private float goX;
-	private float goY;
+
+	private float rotate;
 
 	private boolean remove = false;
 
 	private Texture bulletTexture;
-	private final static int X_MOVE = 30;
-	private final static int Y_MOVE = 30;
+
+	private final static float velocity = -1000;
+	float angle;
+
 	public Bullet(float x, float y, float finalX, float finalY, String stringName) {
 		this.x = x;
 		this.y = y;
@@ -28,14 +30,13 @@ public class Bullet {
 		this.bulletTexture = new Texture(stringName);
 
 		// Calculate the trajectory
-		float xDistance = finalX - x;
-		xDistance = xDistance/X_MOVE;
+		float xDistance = x-finalX;
+		//xDistance = Math.cos(xDistance);
 		
-		float yDistance = finalY - y;
-		yDistance = yDistance/Y_MOVE;
+		float yDistance =  y-finalY;
+		//yDistance = Math.sin(yDistance);
+		angle = MathUtils.atan2(yDistance, xDistance);
 
-		goX = xDistance;
-		goY = yDistance;
 	}
 	
 	public float getX() {
@@ -51,16 +52,25 @@ public class Bullet {
 	}
 
 	private void go() {
-		this.x += goX;
-		this.y += goY;
+		this.x += velocity * Gdx.graphics.getDeltaTime() * MathUtils.cos(angle);
+		this.y += velocity * Gdx.graphics.getDeltaTime() * MathUtils.sin(angle);
 	}
 
 	private float rotate() {
+		if (rotate < 0.0f) {
+			rotate = 360.0f;
+		} else {
+			rotate -= 20;
+		}
 		return 0;
 	}
 
 	public void draw(SpriteBatch batch) {
-		batch.draw(bulletTexture, this.x, this.y);
+		//batch.draw(bulletTexture, this.x, this.y);
+		batch.draw(bulletTexture, this.x, this.y, bulletTexture.getWidth()/2.0f,bulletTexture.getHeight()/2.0f,
+				bulletTexture.getWidth()*1.0f, bulletTexture.getHeight()*1.0f, 1f, 1f, rotate,0,0,bulletTexture.getWidth(),bulletTexture.getHeight(),false,false);
+
+
 	}
 
 	public void update() {
