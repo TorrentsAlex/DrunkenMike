fd8c94946f63556fcb8a915589a2b4c53388559e
alex torrents <torrents.alex1@gmail.com>
2015-11-11 22:34:42 +0100

    Add collision detector with Circles and Rectangles

--- core/src/com/turri/manager/CollisionDetector.java
+++ core/src/com/turri/manager/CollisionDetector.java
@@ -1,8 +1,13 @@
 package com.turri.manager;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.turri.models.Bullet;
 import com.turri.models.Enemy;
 import com.turri.models.Mike;
+import com.turri.models.PowerUp;
 
 /**
  * Created by alex torrents on 01/11/2015.
@@ -14,16 +19,22 @@ public class CollisionDetector {
         if(e.getDead()) {
             return false;
         }
-        if (b.getX() > e.getX() && b.getX() < e.getX() + e.getWidth()) {
-            if (b.getY() > e.getY() && b.getY() < e.getY() + e.getHeight()) {
-                return true;
-            }
-        }
-        return false;
+        return collision(b.getBoundleCircle(), e.getBoundingCircle());
     }
 
     public static boolean getCollision(Mike m, Enemy e) {
+       return collision(m.getBoundingCircle(), e.getBoundingCircle());
+    }
 
-        return false;
+    public static boolean getCollision(Mike mike, PowerUp poweUp) {
+        return collision(mike.getBoundingCircle(), poweUp.getBoundingCircle());
+    }
+
+    // Private method for collisions
+    private static boolean collision(Circle c1, Circle c2) {
+        return Intersector.overlaps(c1, c2);
+    }
+    private static boolean collision(Circle c, Rectangle r) {
+        return Intersector.overlaps(c, r);
     }
 }
